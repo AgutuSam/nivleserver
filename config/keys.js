b09e7741da2901b5a9ea8bddc8fd7b45f8126b5a
AgutuSam <sammyhardcell@gmail.com>
2021-02-20 23:10:37 +0300

    init

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 module.exports = {
-  mongoURI: "mongodb+srv://anuj:anuj1234@cluster0-0gn2z.mongodb.net/test",
+  // mongoURI: "mongodb+srv://anuj:anuj1234@cluster0-0gn2z.mongodb.net/test",
+  // mongoURI: "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false",
+  mongoURI: "mongodb+srv://omondikamau:sbbd4434@cluster0.cudx2.mongodb.net/delivery",
   secretOrKey: "secret",
   userRoles: ["user", "manager"]
 };
